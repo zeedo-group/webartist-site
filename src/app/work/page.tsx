39c38af4118a5cb63694684709b95666e92187f0
zeedo-group <ma.maruf.716@gmail.com>
2026-07-09 15:06:01 +0300

    Update page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -53,7 +53,7 @@ export default function WorkIndex() {
       </div>
 
       <motion.div layout className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <AnimatePresence>
+        <AnimatePresence mode="popLayout" initial={false}>
           {filteredWork.map((item, i) => (
             <motion.div
               layout
